3d0f8d74804db1e29eaa901ada0720c015a9c982
Gabriel Marques <gabriel.marques101@gmail.com>
2023-11-01 18:11:10 -0300

    Calculadora finalizada

---
+++ src/App.js
@@ -0,0 +1,148 @@
+import Input from "./components/Input";
+import Button from "./components/Button";
+
+import { Container, Content, Row, Colum } from "./styles";
+import { useState } from "react";
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState("0");
+  const [firstNumber, setFirstNumber] = useState("0");
+  const [operation, setOperation] = useState("");
+
+  const handleOnClear = () => {
+    setCurrentNumber("0");
+    setFirstNumber("0");
+    setOperation("");
+  };
+
+  const handleAddNumber = (num) => {
+    setCurrentNumber((prev) => `${prev === "0" ? "" : prev}${num}`);
+  };
+
+  const handleSumNumber = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("+");
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation("");
+    }
+    // Função da soma !!!!
+  };
+
+  const handleSubNumber = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("-");
+    } else {
+      const sum = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation("");
+    }
+    // Função da subtração !!!!
+  };
+
+  const handleMultNumber = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("x");
+    } else {
+      const sum = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation("");
+    }
+    // Função da multiplicação !!!!
+  };
+
+  const handleDivNumber = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("÷");
+    } else {
+      const sum = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation("");
+    }
+    // Função da divisão !!!!
+  };
+
+  const handlePorcentNumber = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("%");
+    } else {
+      const sum = (Number(firstNumber) / Number(currentNumber)) * 100;
+      setCurrentNumber(String(sum));
+      setOperation("");
+    }
+    // Função da divisão !!!!
+  };
+
+  const handleEquals = () => {
+    if (firstNumber !== "0" && operation !== "" && currentNumber !== "0") {
+      switch (operation) {
+        case "+":
+          handleSumNumber();
+          break;
+        case "-":
+          handleSubNumber();
+          break;
+        case "x":
+          handleMultNumber();
+          break;
+        case "÷":
+          handleDivNumber();
+          break;
+        case "%":
+          handlePorcentNumber();
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  return (
+    <Container>
+      <Content>
+        <Input value={currentNumber} />
+        <Row>
+          <Button label="AC" onClick={handleOnClear} />
+          <Button label="A" />
+          <Button label="%" onClick={handlePorcentNumber} />
+          <Button label="÷" onClick={handleDivNumber} />
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber("7")} />
+          <Button label="8" onClick={() => handleAddNumber("8")} />
+          <Button label="9" onClick={() => handleAddNumber("9")} />
+          <Button label="x" onClick={handleMultNumber} />
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber("4")} />
+          <Button label="5" onClick={() => handleAddNumber("5")} />
+          <Button label="6" onClick={() => handleAddNumber("6")} />
+          <Button label={"-"} onClick={handleSubNumber} />
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber("1")} />
+          <Button label="2" onClick={() => handleAddNumber("2")} />
+          <Button label="3" onClick={() => handleAddNumber("3")} />
+          <Button label="+" onClick={handleSumNumber} />
+        </Row>
+        <Row>
+          <Button label="0" onClick={() => handleAddNumber("0")} />
+          <Button label="=" onClick={handleEquals} />
+        </Row>
+      </Content>
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+  padding: 20px;
+  border: 1px solid black;
+  background-color: white;
+  color: black;
+  font-size: 24px;
+  font-weight: 700;
+
+  flex: 1;
+
+  &:hover {
+    opacity: 0.5;
+    background-color: #cdcdcd;
+  }
+`;
